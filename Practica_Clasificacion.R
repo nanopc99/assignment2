729d390537ab00dd0cf66208b9b2c495b576368e
lgutie <90090326+lgutie@users.noreply.github.com>
2021-10-06 23:38:58 +0200

    Add files via upload

--- Practica_Clasificacion.R
+++ Practica_Clasificacion.R
@@ -2,24 +2,73 @@
 
 # Librerías
 library(tidyverse)
+library(GGally) # Gráficos de correlación
 diabetes <- read.csv("Diabetes.csv", sep = ";")
 
+#### DATOS #### 
 head(diabetes)
-
-# EDA
-
-summary(diabetes)
+dim(diabetes) # 768 x 9
 any(is.na(diabetes)) # No hay datos missing
 
-apply(diabetes[,-diabetes$DIABETES],2, function (x){
-  ggplot(data = diabetes, mapping = aes(x = diabetes$DIABETES, y = x, fill = diabetes$DIABETES)) + xlab(c)+
-    geom_boxplot()+
-    facet_wrap(~DIABETES)
-    # geom_jitter(width = 0.1, alpha = 0.3)
-  
-})
+####  EDA #### 
 
- 
+####  Resumen numérico #### 
 
+summary(diabetes)
+# Desviación de cada variable
+signif(apply(diabetes, 2, sd),4)
+
+####  Resumen gráfico #### 
+
+#### BOXPLOTS ####
  
- 
+plots <- 
+
+  imap(select( diabetes,PREGNANT:AGE), ~ {
+    ggplot(diabetes, aes(x = factor(DIABETES, labels = c("No", "Si")),
+                        y = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
+      geom_boxplot() +
+      xlab("DIABETES")+ 
+      theme(legend.position = "top", legend.direction = "horizontal")+ 
+      labs (fill = ' DIABETES ') +
+      ylab(.y)
+  })
+
+# Muestra los gráficos
+lapply(plots, function(x) print(x))
+
+
+#### HISOGRAMAS ####
+
+HIST <- imap(select( diabetes,PREGNANT:AGE), ~ {
+    ggplot(diabetes, aes(
+                         x = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
+      geom_histogram(aes(y=stat(density))) +
+                       geom_density(color="red", size=1.5, alpha = 0.2) +
+      xlab("DIABETES")+ 
+      theme(legend.position = "top", legend.direction = "horizontal")+ 
+      labs (fill = ' DIABETES ') +
+      xlab(.y)
+  })
+
+
+
+# Muestra los gráficos
+lapply(HIST, function(x) print(x))
+
+# Vemos si está balanceada la variable respuesta
+
+ggplot(data = diabetes) + 
+  geom_bar(mapping = aes(x = factor(DIABETES, labels = c("No", "Si")),
+                         fill = factor(DIABETES, labels = c("No", "Si"))))+ 
+  labs (fill = ' DIABETES ')+ 
+  xlab("DIABETES")+ theme_bw()+ 
+  theme(legend.position = "top", legend.direction = "horizontal")
+
+
+#### BIVARIATE ANALYSIS ####
+
+ggpairs(diabetes, aes(color = factor(DIABETES, labels = c("No", "Si")), alpha = 0.2))
+
+# Correlación entre variables explicativas para evitar multicolinealidad
+ggcorr(diabetes[, -ncol(diabetes)], method = c("everything", "pearson")) 
