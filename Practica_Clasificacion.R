153d3d28a1871aa1681e1e35292dce410db17ef3
Ignacio Pérez-Cea Espantaleón <nano@MacBook-Pro-de-Ignacio.local>
2021-10-09 11:24:12 +0200

    Regresión logística bien subida

--- Practica_Clasificacion.R
+++ Practica_Clasificacion.R
@@ -27,14 +27,14 @@ signif(apply(diabetes, 2, sd),4)
 
 #### BOXPLOTS ####
 plots <-   imap(select( diabetes,PREGNANT:AGE), ~ {
-    ggplot(diabetes, aes(x = factor(DIABETES, labels = c("No", "Si")),
-                        y = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
-      geom_boxplot() +
-      xlab("DIABETES")+ 
-      theme(legend.position = "top", legend.direction = "horizontal")+ 
-      labs (fill = ' DIABETES ') +
-      ylab(.y)
-  })
+  ggplot(diabetes, aes(x = factor(DIABETES, labels = c("No", "Si")),
+                       y = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
+    geom_boxplot() +
+    xlab("DIABETES")+ 
+    theme(legend.position = "top", legend.direction = "horizontal")+ 
+    labs (fill = ' DIABETES ') +
+    ylab(.y)
+})
 
 # Muestra los gráficos
 lapply(plots, function(x) print(x))
@@ -42,15 +42,15 @@ lapply(plots, function(x) print(x))
 
 #### HISOGRAMAS ####
 HIST <- imap(select( diabetes,PREGNANT:AGE), ~ {
-    ggplot(diabetes, aes(
-                         x = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
-      geom_histogram(aes(y=stat(density))) +
-                       geom_density(color="red", size=1.5, alpha = 0.2) +
-      xlab("DIABETES")+ 
-      theme(legend.position = "top", legend.direction = "horizontal")+ 
-      labs (fill = ' DIABETES ') +
-      xlab(.y)
-  })
+  ggplot(diabetes, aes(
+    x = .x, fill = factor(DIABETES, labels = c("No", "Si")))) + 
+    geom_histogram(aes(y=stat(density))) +
+    geom_density(color="red", size=1.5, alpha = 0.2) +
+    xlab("DIABETES")+ 
+    theme(legend.position = "top", legend.direction = "horizontal")+ 
+    labs (fill = ' DIABETES ') +
+    xlab(.y)
+})
 
 
 
@@ -75,20 +75,38 @@ ggcorr(diabetes[, -ncol(diabetes)], method = c("everything", "pearson"))
 
 #### ALGORITMOS DE MACHINE LEARNING ####
 
-#Creación del train y test set
+#Creación del train y test set y sus conjuntos de evaluación
 set.seed(150) 
 trainIndex <- createDataPartition(diabetes$DIABETES, p = 0.8, list = FALSE, times = 1) 
 train_set <- diabetes[trainIndex,]
 test_set <- diabetes[-trainIndex,]
+train_set_eval <- train_set
+test_set_eval <- test_set
+str(train_set)
 
 #Control del train 
 ctrl <- trainControl(method = "cv", number = 10,summaryFunction = defaultSummary, classProbs = TRUE) 
 
 #Regresión logística
+train_set <- train_set %>%
+  mutate(DIABETES = factor(DIABETES == 1, levels = c(TRUE, FALSE), labels = c("Havediabetes", "Donthavediabetes")))
 LogReg.fit <- train(form = DIABETES ~ ., 
                     data = train_set,              
                     method = "glm",                   
                     preProcess = c("center","scale"), 
                     trControl = ctrl,                 
                     metric = "Accuracy") 
+LogReg.fit
+summary(LogReg.fit) 
+str(LogReg.fit) 
+boxplot(LogReg.fit$resample$Accuracy, xlab = "Accuracy")
+
+#Evaluacuón del modelo
+train_set_eval$LRprob <- predict(LogReg.fit, type="prob", newdata = train_set) # predict probabilities
+train_set_eval$LRpred <- predict(LogReg.fit, type="raw", newdata = train_set) # predict classes 
+#test
+test_set_eval$LRprob <- predict(LogReg.fit, type="prob", newdata = test_set) # predict probabilities
+test_set_eval$LRpred <- predict(LogReg.fit, type="raw", newdata = test_set) # predict classes 
+
+head(train_set)
 
